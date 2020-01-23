f6f60a17c5decc0ea2f186543e249b5d1e27209a
mahmoud.khawatmi <mahmoud.khawatmi@m71-service.de>
2020-01-23 22:48:44 +0100

    add easy admin bundle

---
+++ src/AppBundle/Controller/EasyAdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class EasyAdminController
+ * @package AppBundle\Controller
+ */
+class EasyAdminController extends BaseController
+{
+    /**
+     * @Route("/", name="easyadmin")
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function indexAction(Request $request)
+    {
+        return parent::indexAction($request);
+    }
+}
